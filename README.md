112f4af191c37f19f1633eee13777c9617a7b844
Егор Глебов <beast.loby@gmail.com>
2020-12-17 19:12:42 +0200

    init

--- README.md
+++ README.md
@@ -1,27 +1,20 @@
-# AngularLib
+<h1>Тестовое задание angular-digital-library</h1>
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.0.4.
-
-## Development server
+<h2>Technology</h2>
+<ul>
+     <li> Angular 10 </li>
+     <li> Angular CLI </li>
+     <li> Bootstrap 4 </li>
+     <li> Swiper.js wia ngx-swiper-wrapper </li>
+</ul>
+<p> Layout based on a ready-made template. Free adaptability for iPhone X, iPad Pro, MacBook 16 '. Support for all current browsers </p>
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
-## Code scaffolding
-
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
-## Build
-
 Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
-
-## Running unit tests
 
 Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
 
-## Running end-to-end tests
-
 Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
-
-## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,139 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { SwiperComponent, SwiperDirective } from 'ngx-swiper-wrapper';
+import { SwiperOptions } from 'swiper';
+import { PaginationOptions } from 'swiper/types/components/pagination';
+import { ScrollbarOptions } from 'swiper/types/components/scrollbar';
+import { NavigationOptions } from 'swiper/types/components/navigation';
+
+const slides = [
+  {
+    preview: 'img-carousel-book-2.jpg',
+    descr: 'Bestselling writer Daniel Abraham and acclaimed illustrator Tommy Patterson bring their stunning graphic-novel',
+    link: '#'
+  },
+  {
+    preview: 'img-carousel-book-3.jpg',
+    descr: 'Bestselling writer Daniel Abraham and acclaimed illustrator Tommy Patterson bring their stunning graphic-novel',
+    link: '#'
+  },
+  {
+    preview: 'img-carousel-book-4.jpg',
+    descr: 'Bestselling writer Daniel Abraham and acclaimed illustrator Tommy Patterson bring their stunning graphic-novel',
+    link: '#'
+  },
+  {
+    preview: 'img-carousel-book-1.jpg',
+    descr: 'Bestselling writer Daniel Abraham and acclaimed illustrator Tommy Patterson bring their stunning graphic-novel',
+    link: '#'
+  },
+  {
+    preview: 'img-carousel-book-5.jpg',
+    descr: 'Bestselling writer Daniel Abraham and acclaimed illustrator Tommy Patterson bring their stunning graphic-novel',
+    link: '#'
+  },
+  {
+    preview: 'img-carousel-book-1.jpg',
+    descr: 'Bestselling writer Daniel Abraham and acclaimed illustrator Tommy Patterson bring their stunning graphic-novel',
+    link: '#'
+  },
+  {
+    preview: 'img-carousel-book-2.jpg',
+    descr: 'Bestselling writer Daniel Abraham and acclaimed illustrator Tommy Patterson bring their stunning graphic-novel',
+    link: '#'
+  }
+];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-lib';
+export class AppComponent implements OnInit {
+  public show = true;
+  public slides = slides;
+  public type = 'component';
+  public disabled = false;
+  private windowWidth = document.querySelector('body').offsetWidth;
+  public config: SwiperOptions = {
+    a11y: { enabled: true },
+    direction: 'horizontal',
+    slidesPerView: 4,
+    spaceBetween: 20,
+    centeredSlides: false,
+    keyboard: true,
+    mousewheel: false,
+    scrollbar: false,
+    navigation: true,
+    pagination: false,
+    autoHeight: true
+  };
+  private scrollbar: ScrollbarOptions = {
+    el: '.swiper-scrollbar',
+    hide: false,
+    draggable: true
+  };
+  private pagination: PaginationOptions = {
+    el: '.swiper-pagination',
+    clickable: true,
+    hideOnClick: false
+  };
+
+  // private navigation: NavigationOptions = {
+  //   prevEl: '.custom-button-prev',
+  //   nextEl: '.custom-button-next'
+  // };
+
+  @ViewChild(SwiperComponent, { static: false }) componentRef?: SwiperComponent;
+  @ViewChild(SwiperDirective, { static: false }) directiveRef?: SwiperDirective;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    if (this.windowWidth < 768) {
+      this.config.slidesPerView = 2;
+      // this.config.slidesPerView = 3;
+    } else if (this.windowWidth > 768 && this.windowWidth < 1024) {
+      this.config.slidesPerView = 3;
+      // this.config.slidesPerView = 3;
+    }
+    console.log('hello');
+  }
+
+  public toggleType(): void {
+    this.type = (this.type === 'component') ? 'directive' : 'component';
+  }
+
+  public toggleOverlayControls(): void {
+    if (this.config.navigation) {
+      this.config.scrollbar = false;
+      // this.config.navigation = this.navigation;
+      this.config.pagination = this.pagination;
+    } else if (this.config.pagination) {
+      this.config.navigation = false;
+      this.config.pagination = false;
+
+      this.config.scrollbar = this.scrollbar;
+    } else {
+      this.config.scrollbar = false;
+      this.config.pagination = false;
+
+      this.config.navigation = true;
+    }
+
+    if (this.type === 'directive' && this.directiveRef) {
+      this.directiveRef.setIndex(0);
+    } else if (this.type === 'component' && this.componentRef && this.componentRef.directiveRef) {
+      this.componentRef.directiveRef.setIndex(0);
+    }
+  }
+
+  public onIndexChange(index: number): void {
+    console.log('Swiper index: ', index);
+  }
+
+  public onSwiperEvent(event: string): void {
+    console.log('Swiper event: ', event);
+    console.log(this.windowWidth);
+  }
 }
+
